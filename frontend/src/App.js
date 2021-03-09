4414feee9ad885da59617f14933d3371c3cd69d9
gon tana <gontana1220@gmail.com>
2021-03-09 21:35:06 +0900

    reducer実装

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        
-        </a>
-      </header>
-    </div>
-  );
+const App = () => (<Counter></Counter>)
+
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { count:0 }
+  }
+
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  handleMinButton = () => {
+    this.setState({ count: this.state.count - 1 })
+  }
+
+
+  render() {
+    return (
+    <React.Fragment>
+      <div>count: { this.state.count }</div>
+      <button onClick={this.handlePlusButton}>+1</button>
+      <button onClick={this.handleMinButton}>-1</button>
+    </React.Fragment>
+    )
+  }
 }
 
 export default App;
